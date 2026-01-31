5c432b711c290d1a3e65aed9f4f879ee51a6231f
Lucio Altamirano <laltamirano@e3stores.com>
2026-01-31 03:36:02 -0300

    Se agrego db a teams

--- F1SinGuion/src/lib/services/teamsService.ts
+++ F1SinGuion/src/lib/services/teamsService.ts
@@ -1,32 +1,20 @@
-import { eq } from 'drizzle-orm';
 import { db } from '@lib/db';
 import { teams } from '@lib/schema';
+import { eq } from 'drizzle-orm';
 
 export const getAllTeams = async () => {
   return await db.query.teams.findMany({
     with: {
-      drivers: true,
-    },
+      drivers: true
+    }
   });
-};
+}
 
 export const getTeamBySlug = async (slug: string) => {
-  return await db.query.teams.findFirst({
-    where: eq(teams.index, slug),
-    with: {
-      drivers: true,
-    },
-  });
-};
-
-export const createTeam = async (data: typeof teams.$inferInsert) => {
-  return await db.insert(teams).values(data).returning();
-};
-
-export const updateTeam = async (slug: string, data: Partial<typeof teams.$inferInsert>) => {
-  return await db.update(teams).set(data).where(eq(teams.index, slug)).returning();
-};
-
-export const deleteTeam = async (slug: string) => {
-  return await db.delete(teams).where(eq(teams.index, slug)).returning();
-};
+    return await db.query.teams.findFirst({
+        where: eq(teams.index, slug),
+        with: {
+            drivers: true
+        }
+    })
+}
